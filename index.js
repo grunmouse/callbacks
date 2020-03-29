8dc7f3e242d199f2b8a3c06baa667a336f57a8f4
Dmitry Rakov <rakov@mirapolis.ru>
2020-03-29 20:29:04 +0300

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Объект, накапливающий функции, а потом вызывающий их все с одними и теми же аргументами
+ */
+
+class Callbacks{
+	constructor(){
+		this._callbacks = new Set();
+		this._done = false;
+		
+	}
+	
+	on(callback){
+		if(this._done){
+			callback(this._arg);
+		}
+		else{
+			this._callbacks.add(callback);
+		}
+		return this;
+	}
+	
+	off(callback){
+		this._callbacks.delete(callback);
+	}
+	
+	fire(arg){
+		this._arg = arg;
+		this._done = true;
+		for(let callback of this._callbacks){
+			callback(arg);
+		}
+	}
+}
+
+module.exports = Callbacks;
